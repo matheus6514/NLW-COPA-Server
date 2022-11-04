38b2a9ecbfd874b5e4281c5ac41969be113d4c96
Matheus Gonçalves <matheus_gon97@hotmail.com>
2022-11-04 20:14:47 -0300

    criando rota para listar games

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,6 +1,24 @@
 import { FastifyInstance } from 'fastify';
 import { prisma } from '../lib/prisma';
+import { z } from 'zod';
+import { authenticate } from '../plugins/authenticate';
 
 export async function gameRoutes(fastify: FastifyInstance){
+  fastify.get('/pools/:id/games', {
+    onRequest: [authenticate],
+  }, async (request) => {
+    const getPoolParams = z.object({
+      id: z.string(),
+    })
 
+    const { id } = getPoolParams.parse(request.params)
+    
+    const games = await prisma.game.findMany({
+      orderBy: {
+        date: 'desc',
+      }
+    })
+
+    return { games }
+  })
 }
