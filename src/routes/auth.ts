f9dace8b97be71c8803608b4a8e724e13d966ffa
Matheus Gonçalves <matheus_gon97@hotmail.com>
2022-11-04 07:56:17 -0300

    inserindo token JWT na aplicação

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -46,7 +46,16 @@ export async function authRoutes(fastify: FastifyInstance){
       })
     }
 
-    return { userInfo }
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    }, {
+      sub: user.id,
+      expiresIn: '7 days',
+    }
+    )
+
+    return { token }
 
   })
 
